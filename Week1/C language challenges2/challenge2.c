9d6d9ee36ee661287cf38a85a754483a32721007
medabra <abrache2001@gmail.com>
2022-09-08 15:12:14 +0100

    ajouter les challenge 2

---
+++ Week1/C language challenges2/challenge2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ int a;
+printf("please write the number:");
+scanf("%d",&a);
+if(a%2==0){
+    printf("paire\n");
+}else{
+    printf("impaire\n");
+}
+
+    return 0;
+}
