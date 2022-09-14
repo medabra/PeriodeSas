b4fcf05e31a85a6e13b35aeaf09c9c458fa93976
medabra <abrache2001@gmail.com>
2022-09-14 14:01:24 +0100

    add

---
+++ Week2/challenge  fonction/challenge 1))).c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int med(int ,int );
+   int main(){
+       int a,b,res;
+    printf("please enter the numbers :");
+       scanf("%d",&a);
+       scanf("%d",&b);
+       res= med(a,b);
+printf("the resault is %d",res);
+    return 0;}
+   int med(int a ,int b){
+
+   return a+b;}
